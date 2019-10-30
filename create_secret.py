8ee30bccb48aef5743052a30cdc19b26ba2cfdf8
Rosie Brezynski <robrezyn@microsoft.com>
2019-10-30 19:11:09 -0400

    Initial commit

---
+++ create_secret.py
@@ -0,0 +1,32 @@
+import jwt
+from datetime import datetime
+from datetime import timedelta
+
+key_file = 'key.txt' # file where you stored your private key
+
+# fill i
+team_id = ''
+client_id = ''
+key_id = ''
+
+file = open(key_file)
+
+ecdsa_key = file.read()
+
+headers = {
+        'kid': key_id
+    }
+
+claims = {
+	'iss': team_id,
+	'iat': datetime.now(),
+	'exp': datetime.now() + timedelta(days=180),
+	'aud': 'https://appleid.apple.com',
+	'sub': client_id,
+    }
+
+token = jwt.encode(claims, ecdsa_key, algorithm='ES256', headers=headers)
+
+file.close()
+
+print token
